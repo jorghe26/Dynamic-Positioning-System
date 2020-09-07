18646a5b1344dbb5d1edbdf8867b0ba539dbbcf2
Jørgen <jorghe@stud.ntnu.no>
2020-09-07 15:16:52 +0200

    Naive approach towards plotting position

--- ned2bf.m
+++ ned2bf.m
@@ -1,3 +1,4 @@
 function BF = ned2bf(p)
+    %TODO: Add transformation from NED to bodyframe
     BF = p;
 end
---
+++ visualizeShip.m
@@ -0,0 +1,5 @@
+eta = logsout{1}.Values.Data()
+
+nu = logsout{2}.Values.Data()
+
+plot(eta(:,1),eta(:,2))
