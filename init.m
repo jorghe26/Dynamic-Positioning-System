fdef7f68d5f261fbe27df29f026cfba8acca583b
Haakon <haakon.lyngstad@gmail.com>
2020-09-09 17:51:43 +0200

    First attempt at current model

--- init.m
+++ init.m
@@ -12,8 +12,13 @@ clear all;
 load('supply.mat');
 load('supplyABC.mat');
 load('thrusters_sup.mat')
-
+    
 % Initial position x, y, z, phi, theta, psi
 eta0 = [0,0,0,0,0,0]';
 % Initial velocity u, v, w, p, q, r
 nu0 = [0,0,0,0,0,0]';
+% Velocity multidude 
+V_c = 0.5;
+% Slope for phi change (Should include runtime instead of 1000
+beta_c_slope = -90/(1000);
+beta_c_init = -90;
--- visualizeShip.m
+++ visualizeShip.m
@@ -2,6 +2,8 @@ eta = logsout{1}.Values.Data()
 
 nu = logsout{2}.Values.Data()
 
+
+
 plot(eta(:,1),eta(:,2))
 
 hold on 
