f1128ac478c51cd3f7818d5c6c3e486956d001ce
Haakon <haakon.lyngstad@gmail.com>
2020-09-25 08:23:35 +0200

    Merge changes

--- lqr_error.m
+++ lqr_error.m
@@ -1,4 +1,3 @@
-
 %copied from supply.m
 % Normalization variables
     L    =  76.2;           % length of ship (m)
@@ -22,18 +21,18 @@
     M = (mass*Tinv^2)*(T*Mbis*Tinv);
     D = (mass*Tinv^2)*(sqrt(g/L)*T*Dbis*Tinv);
     
-    Rz =  eye(3,3);
+    I =  eye(3,3);
     
     A_thr = -1/100*eye(3,3);
 
 
-    A = [ zeros(3,3)         Rz     zeros(3,3)
+    A = [ zeros(3,3)         I     zeros(3,3)
           zeros(3,3)    -inv(M)*D      inv(M)
           zeros(3,3)    zeros(3,3)       A_thr];
       
     B = [zeros(3,3); zeros(3,3);  A_thr];
     
-    C = [eye(3,3); zeros(3,3); zeros(3,3)]';
+    C = [I; zeros(3,3); zeros(3,3)]';
     
     Aa = [zeros(3,3)        C
           zeros(9,3)        A ];
@@ -43,23 +42,20 @@
     n_ctr = rank(ctrb(Aa,Ba));
     
     %LQR
-    Q1 = diag([8E9, 8E9, 10E10]); %put penalty on position and heading: eta 
+    Q1 = diag([8E10, 8E10, 15E13]); %put penalty on position and heading: eta 
     Q2 = diag([1000, 1000, 1000]); %put penalty on velocity ν
     Q3 = diag([1, 1, 1]); %put penalty on actuator dynamics τ
     Q = eye(9,9);
     Q(1:3,1:3) = Q1;
     Q(4:6,4:6) = Q2;
     Q(7:9,7:9) = Q3;
-    Qi = diag([8E9, 8E9, 10E10]);
+    Qi = diag([8E10, 8E10, 15E13]);
     Qa = [Qi            zeros(3,9)
           zeros(9,3)         Q    ];
     
     R = eye(3,3);
-    K = lqr(A,B,Q,R);
-    Ki = lqr(Aa,Ba,Qa,R);
-    Ki1 = Ki(1:3,1:3);
-    Ki2 = Ki(1:3,4:6);
-    Ki3 = Ki(1:3,7:9);
-    F = inv(C*inv(B*K-A)*B);
+    K = lqr(Aa,Ba,Qa,R);
+    Ki = K(1:3,1:3);
+    Kp = K(1:3,4:6);
+    Kd = K(1:3,10:12); %% ?
     sim('part1',1000);
-    %run(visualizeShip);
